4a4a6f3cf5ff3467e30fcc1447be0191deef6c53
Your Name <your_email@example.com>
2024-10-08 20:48:29 +1300

    Add out directory for GitHub Pages

---
+++ out/_next/static/cha_hzbOJQPAkthmhV8by/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/657-b1381dddf89ff7f1.js","static/chunks/pages/index-8cefcd483258516c.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
