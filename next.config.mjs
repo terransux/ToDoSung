965d6d4937d13554d6adbf7b685f1900b6b6d790
Your Name <your_email@example.com>
2024-10-08 19:59:54 +1300

    Deploy to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+    };
+  },
 };
 
 export default nextConfig;
